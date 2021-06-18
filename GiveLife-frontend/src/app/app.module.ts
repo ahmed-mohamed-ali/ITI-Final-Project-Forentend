23ed12a944e2fdfd8d38f261fca3c282f0656542
RandaSaadZobair <Rndasaad1@gmail.com>
2021-06-18 19:20:39 +0200

    1st commit

--- GiveLife-frontend/src/app/app.module.ts
+++ GiveLife-frontend/src/app/app.module.ts
@@ -4,9 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Home page component
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+
+//Needer component
+import { CuponComponent } from './components/cupon/cupon.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CuponComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
