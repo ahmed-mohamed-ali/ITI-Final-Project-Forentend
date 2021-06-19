929b32a2f890ee5c3cad1c5e227ffefb5c597e40
ahmed mohamed <ahmedmohamed940@outlook.com>
2021-06-19 22:23:14 +0200

    merge commit

--- GiveLife-frontend/src/app/app.module.ts
+++ GiveLife-frontend/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import{TestComponent} from "./test/test.component"
+import { HttpClientModule } from "@angular/common/http";
+import { JwtService } from './service/jwt.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [JwtService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
