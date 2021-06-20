353578a9f3aaef481f519c65c7008cba61d6afd7
RandaSaadZobair <Rndasaad1@gmail.com>
2021-06-20 17:20:28 +0200

    routing

--- GiveLife-frontend/src/app/app-routing.module.ts
+++ GiveLife-frontend/src/app/app-routing.module.ts
@@ -6,23 +6,19 @@ import { SignInComponent } from './components/sign-in/sign-in.component';
 import { FilterComponent } from './components/filter/filter.component';
 import { CuponComponent } from './components/cupon/cupon.component';
 import { PostComponent } from './components/post/post.component'
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { PostFormComponent } from './components/post-form/post-form.component';
+
 
 const routes: Routes = [
-  {
-    path: 'signup', component: SignUpComponent
-  },
-  {
-    path: 'signin', component: SignInComponent,
-  },
-  {
-    path: 'filter', component: FilterComponent,
-  },
-  {
-    path: 'cupon', component: CuponComponent,
-  },
-  {
-    path: '', component: PostComponent,
-  },
+  { path: '', component: PostComponent},
+  {path: 'signup', component: SignUpComponent},
+  {path: 'signin', component: SignInComponent},
+  {path: 'filter', component: FilterComponent},
+  {path: 'cupon', component: CuponComponent},
+  {path: 'home', component: HomePageComponent},
+  {path: 'postform', component: PostFormComponent},
+  
 ];
 
 @NgModule({
