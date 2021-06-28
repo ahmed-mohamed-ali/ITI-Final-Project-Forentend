56878c72069526842ee28fc05bf03bb979a432d1
esraamelsayed <esraamelsayed223@gmail.com>
2021-06-28 17:38:34 +0200

    update

--- GiveLife-frontend/src/app/components/post/post.component.ts
+++ GiveLife-frontend/src/app/components/post/post.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+//import { Component, OnInit } from '@angular/core';
 import { JwtService } from 'src/app/service/jwt.service';
-
+import { BsModalService, BsModalRef } from "ngx-bootstrap/modal";
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { DonateComponent } from 'src/app/components/donate/donate.component';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
@@ -9,8 +11,33 @@ import { JwtService } from 'src/app/service/jwt.service';
 export class PostComponent implements OnInit {
   subscriber
   postsArray : any
+ // bsModalRef: BsModalRef
+ // private modalService: BsModalService
+  //private changeDetection: ChangeDetectorRef
   constructor(public jwtservice:JwtService) { }
-
+ /* reg(){
+    let bsModalRef = this.modalService.show(DonateComponent);
+    
+  }*/
+ 
+  donateToPost(postId,amountg){
+    
+    this.subscriber=this.jwtservice.donatePost(postId,amountg).subscribe(res =>{
+  
+      let response:any=res;
+      if(response.success){
+        console.log(response);
+    
+      }
+    
+    },err=>{
+      alert(err.message);
+      console.log(err)
+      console.log(err.status)
+      console.log(err.statusText)
+      console.log(err.message)
+    })
+  }
   ngOnInit(): void {
     this.subscriber=this.jwtservice.getRegionPost().subscribe(res =>{
 
@@ -28,6 +55,8 @@ export class PostComponent implements OnInit {
       // console.log(err.statusText)
       // console.log(err.message)
     })
+
+    
   }
 
   // postsArray : any = [
@@ -40,4 +69,6 @@ export class PostComponent implements OnInit {
   //   },
   // ]
 }
+
+
 
