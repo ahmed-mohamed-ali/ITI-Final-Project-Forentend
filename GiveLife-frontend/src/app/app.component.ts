c7d3c2d99f693851fdd37e78344b39b80ce64924
Mayada11 <46032086+Mayada11@users.noreply.github.com>
2021-07-05 19:31:07 +0200

    no internet commit

--- GiveLife-frontend/src/app/app.component.ts
+++ GiveLife-frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConnectionService } from 'ngx-connection-service';
 import { HeaderComponent} from 'src/app/components/header/header.component';
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { HeaderComponent} from 'src/app/components/header/header.component';
 })
 export class AppComponent {
   title = 'GiveLife-frontend';
+   hasNetworkConnection: boolean;
+  hasInternetAccess: boolean;
+  status: string;
+ 
+  constructor(private connectionService: ConnectionService,private router: Router) {
+    this.connectionService.monitor().subscribe(currentState => {
+      this.hasNetworkConnection = currentState.hasNetworkConnection;
+      this.hasInternetAccess = currentState.hasInternetAccess;
+      if (this.hasNetworkConnection && this.hasInternetAccess) {
+        this.status = 'ONLINE';
+        console.log(" internet connection");
+        
+      } else {
+        this.status = 'OFFLINE';
+        
+      }
+     
+    });
+   
+  
+  }
+
 }
+
+
